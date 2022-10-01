2589e41624850d61ccafab6cdcfe3f530cb5b7ea
CEN-Smart <nnanna.echi018@uniport.edu.ng>
2022-10-01 17:42:53 +0100

    More design

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,7 @@
 "use strict";
-
+let str = "+7(903)-123-45-67";
+console.log(str.match(/\d/g).join(""));
+function getMax(arr) {
+	return Math.max(...arr);
+}
+console.log(getMax([1, 2, 3, 12, 1, 23]));
